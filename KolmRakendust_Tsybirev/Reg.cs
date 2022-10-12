8391029dbf0855e5fd139c8689ac94ee95e09a18
opilane <opilane@HP-CZC6297BB7>
2022-10-12 11:11:48 +0300

    database login form

--- KolmRakendust_Tsybirev/Reg.cs
+++ KolmRakendust_Tsybirev/Reg.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -9,21 +10,22 @@ namespace KolmRakendust_Tsybirev
 {
     public partial class Reg : Form
     {
+        TextBox login, pass, mail, vanus, sugu;
         public Reg()
         {
             this.Text = "Menu App";
             this.Size = new Size(600, 500);
             this.BackColor = Color.Gray;
             Label text = new Label { Text = "Registreerimine", Location = new Point(200, 30), AutoSize = true, };
-            TextBox login = new TextBox { Location = new Point(200, 90), Height = 50, Width = 150, };
+             login = new TextBox { Location = new Point(200, 90), Height = 50, Width = 150, };
             Label logintxt = new Label { Text = "Uus Login:", Location = new Point(20, 90), AutoSize = true, };
-            TextBox pass = new TextBox { Location = new Point(200, 130), Height = 50, Width = 150, };
+             pass = new TextBox { Location = new Point(200, 130), Height = 50, Width = 150, };
             Label passtxt = new Label { Text = "Uus parool:", Location = new Point(20, 130), AutoSize = true, };
-            TextBox mail = new TextBox { Location = new Point(200, 170), Height = 50, Width = 150, };
+             mail = new TextBox { Location = new Point(200, 170), Height = 50, Width = 150, };
             Label mailtxt = new Label { Text = "Email:", Location = new Point(20, 170), AutoSize = true, };
-            TextBox vanus = new TextBox { Location = new Point(200, 200), Height = 50, Width = 150, };
+             vanus = new TextBox { Location = new Point(200, 200), Height = 50, Width = 150, };
             Label vanusltxt = new Label { Text = "Vanus:", Location = new Point(20, 200), AutoSize = true, };
-            TextBox sugu = new TextBox { Location = new Point(200, 230), Height = 50, Width = 150, };
+             sugu = new TextBox { Location = new Point(200, 230), Height = 50, Width = 150, };
             Label sugutxt = new Label { Text = "Sugu:", Location = new Point(20, 230), AutoSize = true, };
 
 
@@ -46,10 +48,29 @@ namespace KolmRakendust_Tsybirev
             this.Controls.Add(sugutxt);
             this.Controls.Add(save);
         }
+        public int  I;
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\opilane.TTHK\source\repos\KolmRakendust_Tsybirev\KolmRakendust_Tsybirev\Databas.mdf";
         private void Save_Click(object? sender, EventArgs e)
         {
 
+            SqlConnection connection = new SqlConnection(connectionString);
+            I = 0;
+            connection.Open();
+
+            SqlCommand cmd = connection.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT people(login,pass,mail,vanus,sugu) VALUES ('" + login.Text + "','" + pass.Text + "', '" + mail.Text + "', '" + vanus.Text + "','" + sugu.Text + "');";
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            I = Convert.ToInt32(dt.Rows.Count.ToString());
+            this.Hide();
+            Login mp = new Login();
+            mp.Show();
+
         }
- 
+
     }
+ 
+    
 }
